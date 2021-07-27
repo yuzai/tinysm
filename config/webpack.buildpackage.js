70e7dcaeedb4df3253d4e86aaf214dd04267b82b
maxiaobo01 <maxiaobo01@corp.netease.com>
2021-07-27 19:12:35 +0800

    feat: add npm package

--- config/webpack.buildpackage.js
+++ config/webpack.buildpackage.js
@@ -8,5 +8,22 @@ module.exports = {
       libraryExport: "default",
       library: "tinysm",
       libraryTarget: "umd"
+    },
+    externals: {
+      react: 'react',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }
+      ]
     }
 };
